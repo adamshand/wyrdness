27f7a973d18552493569da4be0b1ec0deb0492d6
Adam Shand <adam@shand.net>
2026-04-28 22:08:21 +1200

    add new favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,15 +5,7 @@
 </script>
 
 <svelte:head>
-	<!-- <link rel="icon" href={favicon} /> -->
-	<link
-		rel="icon"
-		type="image/svg+xml"
-		href="data:image/svg+xml,
-			<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'>
-				<text y='0.9em' font-size='90'>🟣</text>
-			</svg>"
-	/>
+	<link rel="icon" type="image/svg+xml" href={favicon} />
 	<meta name="theme-color" content="#05060a" />
 	<title>WyrdWeb</title>
 </svelte:head>
